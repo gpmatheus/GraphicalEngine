182b95debfabe93375c2640f1884cd21c46d0b96
gpmatheus <gp.matheusluis@gmail.com>
2022-03-25 22:18:10 -0300

    otimizada a chamada de projeção dos vertices

--- src/com/mat/engine/Triangle.java
+++ src/com/mat/engine/Triangle.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Triangle {
     private Vertice[] vertices = new Vertice[3];
+    private List<Vertice> projectedVertices;
 
     public Triangle(Vertice vertice1, Vertice vertice2, Vertice vertice3) {
         vertices[0] = vertice1;
@@ -13,49 +14,58 @@ public class Triangle {
     }
 
     public void rotateX(double alpha, Vertice central) {
+        projectedVertices = null;
         for (Vertice v : vertices) {
             v.rotateX(alpha, central);
         }
     }
 
     public void translateX(double alpha) {
+        projectedVertices = null;
         for (Vertice v : vertices) {
             v.translateX(alpha);
         }
     }
 
     public void rotateY(double alpha, Vertice central) {
+        projectedVertices = null;
         for (Vertice v : vertices) {
             v.rotateY(alpha, central);
         }
     }
 
     public void translateY(double alpha) {
+        projectedVertices = null;
         for (Vertice v : vertices) {
             v.translateY(alpha);
         }
     }
 
     public void rotateZ(double alpha, Vertice central) {
+        projectedVertices = null;
         for (Vertice v : vertices) {
             v.rotateZ(alpha, central);
         }
     }
 
     public void translateZ(double alpha) {
+        projectedVertices = null;
         for (Vertice v : vertices) {
             v.translateZ(alpha);
         }
     }
 
     public void move(double x, double y, double z) {
+        projectedVertices = null;
         for (Vertice v : vertices) {
             v.move(new Vertice(x, y, z));
         }
     }
 
     public List<Vertice> projectedVertices(Projector projector, double res) {
-        List<Vertice> projectedVertices = new ArrayList<>();
+        if (projectedVertices != null)
+            return projectedVertices;
+        projectedVertices = new ArrayList<>();
         for (var vertice : vertices) {
             projectedVertices.add(projector.project(vertice, res));
         }
