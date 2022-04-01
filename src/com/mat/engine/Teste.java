3d04141697692ab80880bffa7fc45f5429a83e73
gpmatheus <gp.matheusluis@gmail.com>
2022-04-01 01:32:49 -0300

    objeto fica inicialmente na posição central

--- src/com/mat/engine/Teste.java
+++ src/com/mat/engine/Teste.java
@@ -11,24 +11,24 @@ public class Teste {
         Vertice vertice2;
         Vertice vertice3;
 
-        vertice1 = new Vertice(-1f, -1f, 2f);
-        vertice2 = new Vertice(1f, -1f, 2f);
-        vertice3 = new Vertice(1f, -1f, 4f);
+        vertice1 = new Vertice(-1f, -1f, -1f);
+        vertice2 = new Vertice(1f, -1f, -1f);
+        vertice3 = new Vertice(1f, -1f, 1f);
         Triangle bottomTriangle = new Triangle(vertice1, vertice2, vertice3);
 
-        vertice1 = new Vertice(-1f, -1f, 2f);
-        vertice2 = new Vertice(1f, 1f, 2f);
-        vertice3 = new Vertice(1f, -1f, 2f);
+        vertice1 = new Vertice(-1f, -1f, -1f);
+        vertice2 = new Vertice(1f, 1f, -1f);
+        vertice3 = new Vertice(1f, -1f, -1f);
         Triangle frontTriangle = new Triangle(vertice1, vertice2, vertice3);
 
-        vertice1 = new Vertice(1f, -1f, 2f);
-        vertice2 = new Vertice(1f, 1f, 2f);
-        vertice3 = new Vertice(1f, -1f, 4f);
+        vertice1 = new Vertice(1f, -1f, -1f);
+        vertice2 = new Vertice(1f, 1f, -1f);
+        vertice3 = new Vertice(1f, -1f, 1f);
         Triangle rightTriangle = new Triangle(vertice1, vertice2, vertice3);
 
-        vertice1 = new Vertice(1f, -1f, 4f);
-        vertice2 = new Vertice(1f, 1f, 2f);
-        vertice3 = new Vertice(-1f, -1f, 2f);
+        vertice1 = new Vertice(1f, -1f, 1f);
+        vertice2 = new Vertice(1f, 1f, -1f);
+        vertice3 = new Vertice(-1f, -1f, -1f);
         Triangle sideTriangle = new Triangle(vertice1, vertice2, vertice3);
         
         Screen screen = new Screen(List.of(bottomTriangle, frontTriangle, rightTriangle, sideTriangle), central);
